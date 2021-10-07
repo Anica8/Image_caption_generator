a4165411d7563d26110275f62b05e6f691e65931
Anica8 <anicasingh8@gmail.com>
2021-10-07 19:35:46 +0530

    final-ig

--- app.py
+++ app.py
@@ -3,20 +3,54 @@ import os
 from os import path
 from pathlib import Path
 import testing_caption_generator as tcg
-
+from db import db,db_init
+from werkzeug.utils import secure_filename
+from db import db_init, db
+from models import Img
 
 app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///img.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db_init(app)
 
 @app.route('/')
 def home_page():
     return render_template("index.html",template_folder='templates')
 
+@app.route('/upload', methods=['POST'])
+def upload():
+    pic = request.files['pic']
+    if not pic:
+        return 'No pic uploaded!', 400
 
-@app.route('/result',methods=['GET'])
-def imageUpload():
-    imgPath=''
-    res=tcg.result(imgPath)
-    return res
+    filename = secure_filename(pic.filename)
+    mimetype = pic.mimetype
+    if not filename or not mimetype:
+        return 'Bad upload!', 400
+
+    img = Img(img=pic.read(), name=filename, mimetype=mimetype)
+    db.session.add(img)
+    db.session.commit()
+    desc=tcg.result(pic)
+    return desc
+    #return 'Img Uploaded!', 200
+
+
+# @app.route('/<int:id>')
+# def get_img(id):
+#     img = Img.query.filter_by(id=id).first()
+#     if not img:
+#         return 'Img Not Found!', 404
+#     print(img.mimetype)
+#     return Response(img.img, mimetype=img.mimetype)
+
+
+# @app.route('/result',methods=['GET'])
+# def imageUpload():
+#     imgPath='image.jpg'
+#     res=tcg.result(imgPath)
+#     return res
 
 
 if __name__ == '__main__':
